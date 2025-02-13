5a76c8917eb659fc4a6ec27f20fb6b8ecb2f7afe
Grant Foster <grantdfoster@gmail.com>
2025-02-13 14:20:28 -0800

    fix: weights

--- validator/weights.py
+++ validator/weights.py
@@ -69,7 +69,7 @@ class WeightsManager:
         blocks_since_update = weights.blocks_since_last_update(
             self.validator.substrate, self.validator.netuid, validator_node_id
         )
-        min_interval = weights._min_interval_to_set_weights(
+        min_interval = weights.min_interval_to_set_weights(
             self.validator.substrate, self.validator.netuid
         )
 
