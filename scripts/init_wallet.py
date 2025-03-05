3d8375ceb9fe0d99135c66984928cdabe7b569e4
5u6r054 <john@masa.finance>
2025-03-05 13:49:13 -0800

    init wallet

--- scripts/init_wallet.py
+++ scripts/init_wallet.py
@@ -13,32 +13,27 @@ def init_wallet():
         if not coldkey_mnemonic:
             raise Exception('COLDKEY_MNEMONIC environment variable is required')
             
-        success = wallet.regenerate_coldkey(
+        # Regenerate coldkey - ignore return value since it outputs success message
+        wallet.regenerate_coldkey(
             mnemonic=coldkey_mnemonic,
             use_password=False,
             overwrite=True
         )
-        if not success:
-            raise Exception('Failed to regenerate coldkey')
 
     # Handle hotkey generation
     auto_generate = os.getenv('AUTO_GENERATE_HOTKEY', '').lower() == 'true'
     hotkey_mnemonic = os.getenv('HOTKEY_MNEMONIC')
 
     if auto_generate:
-        # Generate new hotkey
-        success = wallet.create_new_hotkey(use_password=False)
-        if not success:
-            raise Exception('Failed to generate new hotkey')
+        # Generate new hotkey - ignore return value
+        wallet.create_new_hotkey(use_password=False)
     elif hotkey_mnemonic:
-        # Use provided mnemonic
-        success = wallet.regenerate_hotkey(
+        # Use provided mnemonic - ignore return value
+        wallet.regenerate_hotkey(
             mnemonic=hotkey_mnemonic,
             use_password=False,
             overwrite=True
         )
-        if not success:
-            raise Exception('Failed to regenerate hotkey')
     else:
         msg = ('Either HOTKEY_MNEMONIC must be provided or '
                'AUTO_GENERATE_HOTKEY must be set to true')
