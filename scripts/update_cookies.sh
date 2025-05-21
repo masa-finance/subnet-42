9a36a4edb5037f5f75f809780148b097fcd985a3
Grant Foster <grantdfoster@gmail.com>
2025-05-21 10:13:48 -0700

    fix: worker and cookies

--- scripts/update_cookies.sh
+++ scripts/update_cookies.sh
@@ -1,44 +1,58 @@
 #!/bin/bash
 
 # Environment variables are passed from docker-compose
-# REMOTE_HOST, REMOTE_USER, REMOTE_DIR should be set
+# REMOTE_HOST or COOKIES_REMOTE_HOSTS should be set
+# REMOTE_USER or COOKIES_REMOTE_USER should be set
+# REMOTE_DIR or COOKIES_REMOTE_DIR should be set
 
-echo "Transferring cookies from /app/cookies to $REMOTE_HOST..."
+# Support both singular and plural env var names
+HOSTS=${COOKIES_REMOTE_HOSTS:-$REMOTE_HOST}
+USER=${COOKIES_REMOTE_USER:-$REMOTE_USER}
+DIR=${COOKIES_REMOTE_DIR:-$REMOTE_DIR}
 
-# Create a temporary directory on the remote server
-ssh -i /root/.ssh/id_rsa $REMOTE_USER@$REMOTE_HOST "mkdir -p $REMOTE_DIR"
+# Split hosts by comma if multiple are provided
+IFS=',' read -ra HOST_ARRAY <<< "$HOSTS"
 
-# Copy cookies to the remote server
-scp -i /root/.ssh/id_rsa -r /app/cookies/* $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR/
+for CURRENT_HOST in "${HOST_ARRAY[@]}"; do
+  echo "Transferring cookies from /app/cookies to $CURRENT_HOST..."
 
-# Copy cookies from temporary directory to each worker's volume
-ssh -i /root/.ssh/id_rsa $REMOTE_USER@$REMOTE_HOST "
-  # List files to make sure they were transferred
-  echo 'Files in the temporary directory:'
-  ls -la $REMOTE_DIR/
-  
-  # Find all cookies-volume volumes using a pattern match (both with and without project prefix)
-  echo 'Finding all cookies volumes...'
-  worker_volumes=\$(docker volume ls --format '{{.Name}}' | grep -E '(miner-[0-9]+_)?cookies-volume')
-  
-  if [ -z \"\$worker_volumes\" ]; then
-    echo 'No cookies volumes found! Are the miners running?'
-  else
-    # Update each volume found
-    echo \"\$worker_volumes\" | while read volume; do
-      echo \"Updating volume: \$volume\"
-      docker run --rm -v \"\$volume\":/volume -v $REMOTE_DIR:/source --user root alpine sh -c \"
-        # Update only JSON files in the volume, don't touch other files
-        echo 'Copying JSON files to volume...'
-        cp -v /source/*.json /volume/
-        echo 'Files in the volume:'
-        ls -la /volume/*.json 2>/dev/null || echo 'No JSON files found'
-      \"
-    done
-  fi
-  
-  # Clean up temporary directory
-  rm -rf $REMOTE_DIR
-"
+  # Create a temporary directory on the remote server
+  ssh -i /root/.ssh/id_rsa $USER@$CURRENT_HOST "mkdir -p $DIR"
 
-echo "Cookies successfully updated in all miner volumes!" 
+  # Copy cookies to the remote server
+  scp -i /root/.ssh/id_rsa -r /app/cookies/* $USER@$CURRENT_HOST:$DIR/
+
+  # Copy cookies from temporary directory to each worker's volume
+  ssh -i /root/.ssh/id_rsa $USER@$CURRENT_HOST "
+    # List files to make sure they were transferred
+    echo 'Files in the temporary directory:'
+    ls -la $DIR/
+    
+    # Find all cookies-volume volumes using a pattern match (both with and without project prefix)
+    echo 'Finding all cookies volumes...'
+    worker_volumes=\$(docker volume ls --format '{{.Name}}' | grep -E '(miner-[0-9]+_)?cookies-volume')
+    
+    if [ -z \"\$worker_volumes\" ]; then
+      echo 'No cookies volumes found! Are the miners running?'
+    else
+      # Update each volume found
+      echo \"\$worker_volumes\" | while read volume; do
+        echo \"Updating volume: \$volume\"
+        docker run --rm -v \"\$volume\":/volume -v $DIR:/source --user root alpine sh -c \"
+          # Update only JSON files in the volume, don't touch other files
+          echo 'Copying JSON files to volume...'
+          cp -v /source/*.json /volume/
+          echo 'Files in the volume:'
+          ls -la /volume/*.json 2>/dev/null || echo 'No JSON files found'
+        \"
+      done
+    fi
+    
+    # Clean up temporary directory
+    rm -rf $DIR
+  "
+
+  echo "Cookies successfully updated in all miner volumes on $CURRENT_HOST!"
+done
+
+echo "All remote cookie updates completed!" 
