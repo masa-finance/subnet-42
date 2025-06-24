b88b28af0c25a06e4caa4e8bb95b3bcfb4d72cd6
hide-on-bush-x <hideonbushx123@gmail.com>
2025-06-24 11:50:48 -0300

    feat: caching nats and resending every 5 minutes

--- validator/background_tasks.py
+++ validator/background_tasks.py
@@ -109,7 +109,7 @@ class BackgroundTasks:
                     self.validator.routing_table_updating = False
 
                 # Now safely publish to NATS with consistent data
-                await self.validator.NATSPublisher.send_connected_nodes()
+                await self.validator.NATSPublisher.send_connected_nodes(force=True)
 
                 # Also publish priority miners sorted by score
                 # await self.validator.NATSPublisher.send_priority_miners()
